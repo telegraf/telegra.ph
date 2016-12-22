0ae4af7225be41cb8f09b1b33f4b5c825da50a6c
Vitaly Domnikov <dotcypress@gmail.com>
2016-12-21 18:31:37 -0800

    initial version

---
+++ index.js
@@ -0,0 +1,95 @@
+const fetch = require('isomorphic-fetch')
+
+class Telegraph {
+  constructor (token, opts) {
+    this.options = Object.assign({
+      token: token,
+      apiRoot: 'https://api.telegra.ph'
+    }, opts)
+  }
+
+  set token (token) {
+    this.options.token = token
+  }
+
+  callService (method, payload) {
+    return fetch(`${this.options.apiRoot}/${method}`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(payload)
+    })
+    .then(unwrap)
+  }
+
+  createAccount (shortName, name, url) {
+    return this.callService('createAccount', {
+      short_name: shortName,
+      author_name: name,
+      author_url: url
+    })
+  }
+
+  createPage (title, content, authorName, authorUrl, returnContent) {
+    return this.callService('createPage', {
+      access_token: this.options.token,
+      title: title,
+      author_name: authorName,
+      author_url: authorUrl,
+      content: content,
+      return_content: returnContent
+    })
+  }
+
+  editPage (path, title, content, authorName, authorUrl, returnContent) {
+    return this.callService(`editPage/${path}`, {
+      access_token: this.options.token,
+      title: title,
+      author_name: authorName,
+      author_url: authorUrl,
+      content: content,
+      return_content: returnContent
+    })
+  }
+
+  getPage (path, returnContent) {
+    return this.callService(`getPage/${path}`, {
+      access_token: this.options.token,
+      return_content: returnContent
+    })
+  }
+
+  getViews (path, year, month, day, hour) {
+    return this.callService(`getViews/${path}`, {
+      access_token: this.options.token,
+      year: year,
+      month: month,
+      day: day,
+      hour: hour
+    })
+  }
+
+  getPageList (path, offset, limit) {
+    return this.callService('getPageList', {
+      access_token: this.options.token,
+      offset: offset,
+      limit: limit
+    })
+  }
+}
+
+function unwrap (res) {
+  if (!res.ok) {
+    const err = new Error(res.statusText || 'Error calling telegra.ph')
+    err.statusCode = res.status
+    throw err
+  }
+  return res.json()
+    .then((json) => {
+      if (('ok' in json && !json.ok)) {
+        throw new Error(json.error || 'Error calling telegra.ph')
+      }
+      return json.result
+    })
+}
+
+module.exports = Telegraph
---
+++ readme.md
@@ -0,0 +1,55 @@
+# 📝 telegra.ph
+[![NPM Version](https://img.shields.io/npm/v/telegra.ph.svg?style=flat-square)](https://www.npmjs.com/package/telegra.ph)
+[![Build Status](https://img.shields.io/travis/dotcypress/telegra.ph.svg?branch=master&style=flat-square)](https://travis-ci.org/telegraf/telegra.ph)
+
+Tiny API helper for Telegra.ph.
+
+[Telegra.ph API docs](http://telegra.ph/api)
+
+## API
+
+## Installation
+
+```js
+$ npm install telegra.ph --save
+```
+
+## Example
+
+```js
+const Telegraph = require('telegra.ph')
+
+const client = new Telegraph()
+
+client.createAccount().then((account) => {
+  client.token = account.access_token
+  return client.getPageList()
+}).then((pages) => console.log(pages))
+
+```
+
+```js
+const Telegraph = require('telegra.ph')
+
+const client = new Telegraph(process.env.TOKEN)
+client.getPageList().then((pages) => console.log(pages))
+
+```
+
+## API documentation
+
+
+#### [`createAccount`](http://telegra.ph/api#createAccount)
+`.createAccount(shortName, name, url)`
+
+#### [`createPage`](http://telegra.ph/api#createPage)
+`.createPage(title, content, authorName, authorUrl, returnContent)`
+
+#### [`editPage`](http://telegra.ph/api#editPage)
+`.editPage(path, title, content, authorName, authorUrl, returnContent)`
+
+#### [`getPage`](http://telegra.ph/api#getPage)
+`.getPage(path, returnContent)`
+
+#### [`getPageList`](http://telegra.ph/api#getPageList)
+`.getPageList(path, offset, limit)`
